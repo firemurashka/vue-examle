9302ebb623a003df4d3357781978867f4518a110
firemurashka <firemurashka@gmail.com>
2022-07-04 14:26:57 +0300

    likes and dislikes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,33 @@
 <template>
-Работает
+	<div>
+		<button @click="addLike">Like</button>
+		<button @click="addDislike">Dislike</button>
+	</div>
+	<!-- {{модель}} синтаксис интерполяции - обращение к модели -->
+	<div>Кол-во лайков: <strong>{{ likes }}</strong></div>
+	<div>Кол-во дизлайков: <strong>{{ dislikes }}</strong></div>
+
 </template>
 
 <script>
 
+export default {
+	data() {
+		return {
+			likes: 0,
+			dislikes: 0,
+		}
+	},
+	methods: {
+		addLike() {
+			this.likes += 1;
+		},
+		addDislike() {
+			this.dislikes += 1;
+		}
+	}
+}
 </script>
 
 <style>
-
 </style>
