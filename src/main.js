15c733ea8ce2ecebe115269ca96bc46990a0f7b1
firemurashka <firemurashka@gmail.com>
2022-07-04 13:58:28 +0300

    создание App

--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+//createApp - функция, с помощью которой создается экземпляр приложения
+import {
+    createApp
+} from 'vue'
 
+import App from './App'
+//параметром в эту функцию передаем корневой компонент App
+//mount - возвращает экземпляр корневого компонента
 createApp(App).mount('#app')
+    //#app - куда мы монтируем приложение в div с id=app в index.html
